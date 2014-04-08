8675006b125a7cde9c0814d9c0828310c45ccfe5
Mischa ter Smitten <mischa@tersmitten.nl>
2014-04-08 16:39:29 +0200

    Added test for failing single user import

--- Test/Case/Model/EdexmlTest.php
+++ Test/Case/Model/EdexmlTest.php
@@ -369,4 +369,71 @@ class EdexmlTest extends CakeTestCase {
 		$this->assertEqual(count($result['SchoolClass']), count($data['EDEX']['groepen']['groep']));
 		$this->assertEqual(count($result['Teacher']), count($data['EDEX']['leerkrachten']['leerkracht']));
 	}
+
+/**
+ * testConvertSingleItemData method
+ *
+ * @return void
+ */
+	public function testConvertSingleItemData() {
+		$data = array(
+			'EDEX' => array(
+				'school' => array(
+					'schooljaar' => '2012-2013',
+					'brincode' => '99XX',
+					'dependancecode' => '99',
+					'schoolkey' => '99999',
+					'aanmaakdatum' => '2013-03-27T12:43:06',
+					'auteur' => 'Cito LeerlingVolgSysteem versie 4.6',
+					'xsdversie' => '1.03'
+				),
+				'groepen' => array(
+					'groep' => array(
+						'@key' => '49',
+						'naam' => '1A',
+						'jaargroep' => '1',
+						'mutatiedatum' => '2005-08-29T22:19:18'
+					)
+				),
+				'leerlingen' => array(
+					'leerling' => array(
+						'@key' => '3580',
+						'achternaam' => 'Achternaam',
+						'voorvoegsel' => 'v. d.',
+						'roepnaam' => 'Roepnaam',
+						'geboortedatum' => '2009-08-07',
+						'geslacht' => '2',
+						'jaargroep' => '4',
+						'etniciteit' => '9',
+						'land' => '?',
+						'land_vader' => '?',
+						'land_moeder' => '?',
+						'gewicht_nieuw' => '?',
+						'gewicht' => '?',
+						'mutatiedatum' => '2011-01-24T15:33:29'
+					)
+				),
+				'leerkrachten' => array(
+					'leerkracht' => array(
+						'@key' => '75',
+						'achternaam' => 'Achternaam',
+						'roepnaam' => 'Roepnaam',
+						'groepen' => array(
+							'groep' => array(
+								'@key' => '49'
+							)
+						),
+						'mutatiedatum' => '2004-02-07T14:03:18'
+					)
+				),
+				'@xsi:noNamespaceSchemaLocation' => 'EDEXML.structuur.xsd'
+			)
+		);
+
+		$result = $this->Edexml->convert($data);
+		$this->assertTrue(Hash::numeric(array_keys($result['SchoolClass'])));
+		$this->assertTrue(Hash::numeric(array_keys($result['Student'])));
+		$this->assertTrue(Hash::numeric(array_keys($result['Teacher'])));
+	}
+
 }
